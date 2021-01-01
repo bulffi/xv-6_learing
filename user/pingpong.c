653d5a00cb99e338f532834a6cf4d5c1a68ddcb4
Zijian <39160610+bulffi@users.noreply.github.com>
2021-01-01 11:42:29 +0800

    feat: pingpong is done

--- user/pingpong.c
+++ user/pingpong.c
@@ -3,6 +3,27 @@
 
 int main(int argc, char const *argv[])
 {
-    write(1, "pingpong", 8);    
+    int pipes[2];
+    pipe(pipes);
+    printf("%d %d\n", pipes[0], pipes[1]);
+    int pid = fork();
+    if (pid > 0) {
+        // int father_pid = getpid();
+        write(pipes[1], "HELLO", 5);
+        printf("Write is done");
+        wait(&pid);
+        printf("In father\n");
+        exit(0);
+    } else if (pid == 0) {
+        // int child_pid = getpid();
+        printf("In child\n");
+        char buf;
+        while (read(pipes[0], &buf, 1)) {
+            printf("%c", buf);
+        }
+        exit(0);
+    } else {
+        printf("Error in fork\n");
+    }
     exit(0);
 }
